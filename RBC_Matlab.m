8a2c462bf5fcf541ac49a54838045db2bcec635f
Sam Boysel <sboysel@gmail.com>
2017-11-17 15:56:23 -0800

    a7, q1 and q2

--- RBC_Matlab.m
+++ RBC_Matlab.m
@@ -1,7 +1,11 @@
-%% Basic RBC model with full depreciation
+%% Stochastic NGM
 %
-% Jesus Fernandez-Villaverde
+% Attribution: Jesus Fernandez-Villaverde
 % Haverford, July 3, 2013
+% Source: https://github.com/jesusfv/Comparison-Programming-Languages-Economics/blob/master/RBC_Matlab.m
+%
+% Adapted for ECON 602, Assignment 7 by
+% Sam Boysel
 
 %% 0. Housekeeping
 
@@ -13,30 +17,28 @@ tic
 
 %%  1. Calibration
 
-aalpha = 1/3;     % Elasticity of output w.r.t. capital
-bbeta  = 0.95;    % Discount factor
+alpha = 0.36;    % Elasticity of output w.r.t. capital
+beta  = 0.96;    % Discount factor
 
 % Productivity values
-vProductivity = [0.9792; 0.9896; 1.0000; 1.0106; 1.0212]';
+vProductivity = [exp(-sqrt(0.007^2/(1 - 0.95^2)));
+                 exp(sqrt(0.007^2/(1 - 0.95^2)))]';
 
 % Transition matrix
-mTransition   = [0.9727, 0.0273, 0.0000, 0.0000, 0.0000;
-                 0.0041, 0.9806, 0.0153, 0.0000, 0.0000;
-                 0.0000, 0.0082, 0.9837, 0.0082, 0.0000;
-                 0.0000, 0.0000, 0.0153, 0.9806, 0.0041;
-                 0.0000, 0.0000, 0.0000, 0.0273, 0.9727];
+mTransition = [0.975, 0.025;
+               0.025, 0.975];
 
 %% 2. Steady State
 
-capitalSteadyState = (aalpha*bbeta)^(1/(1-aalpha));
-outputSteadyState = capitalSteadyState^aalpha;
+capitalSteadyState = (alpha*beta)^(1/(1-alpha));
+outputSteadyState = capitalSteadyState^alpha;
 consumptionSteadyState = outputSteadyState-capitalSteadyState;
 
-fprintf(' Output = %2.6f, Capital = %2.6f, Consumption = %2.6f\n', outputSteadyState, capitalSteadyState, consumptionSteadyState); 
+fprintf(' y_ss = %2.6f, k_ss = %2.6f, c_ss = %2.6f\n', outputSteadyState, capitalSteadyState, consumptionSteadyState);
 fprintf('\n')
 
 % We generate the grid of capital
-vGridCapital = 0.5*capitalSteadyState:0.00001:1.5*capitalSteadyState;
+vGridCapital = 0.95*capitalSteadyState:0.00001:1.05*capitalSteadyState;
 
 nGridCapital = length(vGridCapital);
 nGridProductivity = length(vProductivity);
@@ -51,7 +53,7 @@ expectedValueFunction = zeros(nGridCapital,nGridProductivity);
 
 %% 4. We pre-build output for each point in the grid
 
-mOutput = (vGridCapital'.^aalpha)*vProductivity;
+mOutput = (vGridCapital'.^alpha)*vProductivity;
 
 %% 5. Main iteration
 
@@ -59,56 +61,56 @@ maxDifference = 10.0;
 tolerance = 0.0000001;
 iteration = 0;
 
-while (maxDifference>tolerance)  
-    
+while (maxDifference>tolerance)
+
     expectedValueFunction = mValueFunction*mTransition';
-    
+
     for nProductivity = 1:nGridProductivity
-        
+
         % We start from previous choice (monotonicity of policy function)
         gridCapitalNextPeriod = 1;
-        
+
         for nCapital = 1:nGridCapital
-                        
+
             valueHighSoFar = -1000.0;
             capitalChoice  = vGridCapital(1);
-            
+
             for nCapitalNextPeriod = gridCapitalNextPeriod:nGridCapital
-                
+
                 consumption = mOutput(nCapital,nProductivity)-vGridCapital(nCapitalNextPeriod);
-                valueProvisional = (1-bbeta)*log(consumption)+bbeta*expectedValueFunction(nCapitalNextPeriod,nProductivity);
-            
+                valueProvisional = log(consumption)+beta*expectedValueFunction(nCapitalNextPeriod,nProductivity);
+
                 if (valueProvisional>valueHighSoFar)
                     valueHighSoFar = valueProvisional;
                     capitalChoice = vGridCapital(nCapitalNextPeriod);
                     gridCapitalNextPeriod = nCapitalNextPeriod;
                 else
                     break; % We break when we have achieved the max
-                end    
-                  
+                end
+
             end
-            
+
             mValueFunctionNew(nCapital,nProductivity) = valueHighSoFar;
             mPolicyFunction(nCapital,nProductivity) = capitalChoice;
-            
+
         end
-        
+
     end
-    
+
     maxDifference = max(max(abs(mValueFunctionNew-mValueFunction)));
     mValueFunction = mValueFunctionNew;
-    
+
     iteration = iteration+1;
     if (mod(iteration,10)==0 || iteration ==1)
-        fprintf(' Iteration = %d, Sup Diff = %2.8f\n', iteration, maxDifference); 
+        fprintf(' Iteration = %d, Sup Diff = %2.8f\n', iteration, maxDifference);
     end
-           
+
 end
 
-fprintf(' Iteration = %d, Sup Diff = %2.8f\n', iteration, maxDifference); 
+fprintf(' Iteration = %d, Sup Diff = %2.8f\n', iteration, maxDifference);
 fprintf('\n')
 
-fprintf(' My check = %2.6f\n', mPolicyFunction(1000,3)); 
+fprintf(' My check = %2.6f\n', mPolicyFunction(1000,2));
 fprintf('\n')
 
 toc
@@ -119,19 +121,26 @@ figure(1)
 
 subplot(3,1,1)
 plot(vGridCapital,mValueFunction)
+xlabel('k')
+ylabel('V(k, z)')
 xlim([vGridCapital(1) vGridCapital(nGridCapital)])
 title('Value Function')
+legend('k_l','k_h','Location','southeast')
 
 subplot(3,1,2)
 plot(vGridCapital,mPolicyFunction)
+xlabel('k')
+ylabel("k'")
 xlim([vGridCapital(1) vGridCapital(nGridCapital)])
 title('Policy Function')
+legend('k_l','k_h','Location','southeast')
 
-vExactPolicyFunction = aalpha*bbeta.*(vGridCapital.^aalpha);
-
-subplot(3,1,3)
-plot((100.*(vExactPolicyFunction'-mPolicyFunction(:,3))./mPolicyFunction(:,3)))
-title('Comparison of Exact and Approximated Policy Function')
+% vExactPolicyFunction = alpha*beta.*(vGridCapital.^alpha);
+% 
+% subplot(3,1,3)
+% plot((100.*(vExactPolicyFunction'-mPolicyFunction(:,2))./mPolicyFunction(:,2)))
+% xlabel('Iteration')
+% title('Comparison of Exact and Approximated Policy Function')
 
 %set(gcf,'PaperOrientation','landscape','PaperPosition',[-0.9 -0.5 12.75 9])
 %print('-dpdf','Figure1.pdf')
